81776fc1b66ceb24310b074f012286ad8d58ccfe
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2025-10-21 10:36:12 -0400

    Update neuralprophet_time_series_forecast.py

--- src/neuralprophet_time_series_forecast.py
+++ src/neuralprophet_time_series_forecast.py
@@ -20,3 +20,10 @@ print(forecast[["ds", "yhat1"]].tail())
 
 # We're watching a neural network learn patterns in time series data,
 # then generate future predictions.
+#            ds       yhat1
+# 25 2023-05-06  185.279083
+# 26 2023-05-07  126.233025
+# 27 2023-05-08  137.278748
+# 28 2023-05-09  148.293991
+# 29 2023-05-10  159.332321
+
